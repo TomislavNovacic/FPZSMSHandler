26e4ac92b9242d87cb56f76885513bbca6af5f54
Tomi <tomislav.novacic2@gmail.com>
2017-11-12 15:44:11 +0100

    final version of the app

--- app/src/main/java/com/example/android/fpzsmshandler/DialogActivity.java
+++ app/src/main/java/com/example/android/fpzsmshandler/DialogActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.fpzsmshandler;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
@@ -9,11 +13,17 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.HashMap;
+import java.util.Locale;
+
 /**
  * Created by tnovacic on 06.11.2017..
  */
 
 public class DialogActivity extends AppCompatActivity {
+
+    TextToSpeech tts;
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +33,7 @@ public class DialogActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
         Intent intent = getIntent();
-        String message = (String) intent.getExtras().get("Message");
+        message = (String) intent.getExtras().get("Message");
 
         TextView messageBox = (TextView) findViewById(R.id.message_box);
         messageBox.setText(message);
@@ -36,5 +46,45 @@ public class DialogActivity extends AppCompatActivity {
             }
         });
 
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if (status != TextToSpeech.ERROR) {
+                            tts.setLanguage(Locale.ENGLISH);
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                ttsGreater21(message);
+                            } else {
+                                ttsUnder20(message);
+                            }
+                        }
+                    }
+                });
+            }
+        }, 1500);
+    }
+
+    public void onPause() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
+    }
+
+    @SuppressWarnings("deprecation")
+    private void ttsUnder20(String text) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void ttsGreater21(String text) {
+        String utteranceId = this.hashCode() + "";
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
     }
 }
--- app/src/main/java/com/example/android/fpzsmshandler/MainActivity.java
+++ app/src/main/java/com/example/android/fpzsmshandler/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -24,8 +25,6 @@ import static android.Manifest.permission.BROADCAST_SMS;
 import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,72 +48,23 @@ public class MainActivity extends AppCompatActivity {
         intent2.putExtra("Message", messageText);
         startActivity(intent2);
 
-        SMSReceiver.bindListener(new SMSListener() {
+       SMSReceiver.bindListener(new SMSListener() {
             @Override
             public void messageReceived(final String messageText) {
-
-
-                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-                    @Override
-                    public void onInit(int status) {
-                        if (status != TextToSpeech.ERROR) {
-                            tts.setLanguage(Locale.ENGLISH);
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                                ttsGreater21(messageText);
-                            } else {
-                                ttsUnder20(messageText);
-                            }
-                        }
-                    }
-                }); 
-
-                Intent intent = new Intent(getApplicationContext(), DialogActivity.class);
-                intent.putExtra("Message", messageText);
-                startActivity(intent);
-
-
-             /*   Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
-                notificationIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                TaskStackBuilder stackBuilder = TaskStackBuilder.create(getApplicationContext());
-                stackBuilder.addParentStack(SamoZaProbu.class);
-                stackBuilder.addNextIntent(notificationIntent);
-                PendingIntent notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT); */
-
-               /* NotificationCompat.Builder mBuilder =
-                        (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
-                                .setSmallIcon(R.drawable.ic_launcher)
-                                .setContentTitle("FPZ Traffic information")
-                               // .setContentIntent(notificationPendingIntent)
-                               // .setDefaults(Notification.DEFAULT_ALL)
-                                .setPriority(Notification.PRIORITY_MAX)
-                                .setStyle(new NotificationCompat.BigTextStyle().bigText(messageText))
-                                .setContentText(messageText);
-
-                NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-                notificationManager.notify(1, mBuilder.build()); */
-            }
+                SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+                String previousMessage = prefs.getString("Poruka", "");
+                if(!(messageText.equals(previousMessage))) {
+                    SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+                    editor.clear();
+                    editor.apply();
+                    editor.putString("Poruka",messageText);
+                    editor.apply();
+                    Intent intent = new Intent(getApplicationContext(), DialogActivity.class);
+                    intent.putExtra("Message", messageText);
+                    startActivity(intent);
+                }
+               }
         });
-    }
-
-    public void onPause() {
-        if (tts != null) {
-            tts.stop();
-            tts.shutdown();
-        }
-        super.onPause();
-    }
-
-    @SuppressWarnings("deprecation")
-    private void ttsUnder20(String text) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "MessageId");
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, map);
-    }
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    private void ttsGreater21(String text) {
-        String utteranceId = this.hashCode() + "";
-        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
     }
 
     public void createPermissions(){
