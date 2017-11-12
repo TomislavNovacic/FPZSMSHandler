3806e52e9ec804353923e954babd0527f730c09f
Tomi <tomislav.novacic2@gmail.com>
2017-11-12 14:48:56 +0100

    message shown on lock screen

--- app/src/main/java/com/example/android/fpzsmshandler/DialogActivity.java
+++ app/src/main/java/com/example/android/fpzsmshandler/DialogActivity.java
@@ -3,6 +3,10 @@ package com.example.android.fpzsmshandler;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 /**
@@ -15,11 +19,22 @@ public class DialogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_layout);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
         Intent intent = getIntent();
         String message = (String) intent.getExtras().get("Message");
 
         TextView messageBox = (TextView) findViewById(R.id.message_box);
         messageBox.setText(message);
+
+        Button button = (Button) findViewById(R.id.ok_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/android/fpzsmshandler/MainActivity.java
+++ app/src/main/java/com/example/android/fpzsmshandler/MainActivity.java
@@ -1,19 +1,27 @@
 package com.example.android.fpzsmshandler;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
 import java.util.HashMap;
 import java.util.Locale;
+
+import static android.Manifest.permission.BROADCAST_SMS;
+import static java.security.AccessController.getContext;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +31,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.e("Message from main", "hmmm");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            createPermissions();
+        }
+
+        PowerManager.WakeLock screenLock = ((PowerManager)getSystemService(POWER_SERVICE)).newWakeLock(
+                PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "TAG");
+        screenLock.acquire();
+
+        screenLock.release();
+               Log.e("Message from main", "hmmm");
 
         Intent intent = getIntent();
         String messageText = (String) intent.getExtras().get("MessageFromService");
@@ -37,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
             public void messageReceived(final String messageText) {
 
 
-           /*     tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+                tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
                     @Override
                     public void onInit(int status) {
                         if (status != TextToSpeech.ERROR) {
@@ -49,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     }
-                }); */
+                }); 
 
                 Intent intent = new Intent(getApplicationContext(), DialogActivity.class);
                 intent.putExtra("Message", messageText);
@@ -98,5 +115,16 @@ public class MainActivity extends AppCompatActivity {
     private void ttsGreater21(String text) {
         String utteranceId = this.hashCode() + "";
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+
+    public void createPermissions(){
+        String permission = Manifest.permission.READ_SMS;
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED){
+            if(!ActivityCompat.shouldShowRequestPermissionRationale(this, permission)){
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{permission}, 1);
+                }
+            }
+        }
     }
 }
